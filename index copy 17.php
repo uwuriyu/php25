07ca55a2865a8e5da7f665e3df620cc84eb88697
uwuriyu <163380486+uwuriyu@users.noreply.github.com>
2024-03-27 09:44:07 +0500

    Add files via upload

---
+++ index copy 17.php
@@ -0,0 +1,11 @@
+<?php
+function printName() {
+    $firstName = "Гульшат";
+    $lastName = "Темирханова";
+    echo "Имя: " . $firstName . "<br>";
+    echo "Фамилия: " . $lastName;
+}
+
+// Вызов функции для вывода имени и фамилии на экран
+printName();
+?>
